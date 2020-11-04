50818a951b224fa1ee31210e2c1eed35b8b00633
Adir Avraham <adir@koch.local>
2020-11-04 18:38:19 +0200

    black-bg

--- src/components/home-page/index.tsx
+++ src/components/home-page/index.tsx
@@ -7,12 +7,16 @@ const { Title } = Typography;
 export default function home() {
 
     return (
+
+
        <div className="bg-home">
             <Row justify="center" align="middle">
                 <Col>
                     <Title>משרד עורכי דין אסתי קוך</Title>
+                    <h1>משרד עורכי דין אסתי קוך</h1>
                 </Col>
             </Row>
        </div> 
+
     )
 }
